4be1544d10f1ed2819620c461f9260a3067d7cc3
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-01-27 10:55:11 -0500

    robotic arm sync

--- main.py
+++ main.py
@@ -1,22 +1,57 @@
-from flask import Flask, render_template, abort, request,\
-    redirect, url_for, flash, send_file, jsonify
+from sys import stdout
+import logging
+from redis_interface import set_progress_text, get_progress_text, get_progress_cells
+logging.basicConfig(level=logging.ERROR, filename='error.log')
+logger = logging.getLogger(__name__)
+import logging
+from flask import Flask, render_template, Response, request, session
+from flask_socketio import SocketIO, emit
+from time import sleep
 
-global app
+
+clients=[]
+
+
+
+PROCESS_START_PROCESS = None
+
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(stdout))
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+app.config['cors_allowed_origins'] = "*"
+socketio = SocketIO(app)
 
+#lock = multiprocessing.Lock()
 FLAG_HOLDER = {}
+
+
+
 
 
 def instantiate_flags():
     FLAG_HOLDER['mode'] = ""
     FLAG_HOLDER['manual_operation'] = ""
     FLAG_HOLDER['in_execution'] = 0
+    FLAG_HOLDER['img1'] = "static/assets/progress_icons/start.png"
+    FLAG_HOLDER['img2'] = "static/assets/progress_icons/start.png"
+    FLAG_HOLDER['img3'] = "static/assets/progress_icons/start.png"
+    FLAG_HOLDER['img4'] = "static/assets/progress_icons/start.png"
+
+
+def broadcast_progress():
+    dict=get_progress_cells()
+    socketio.emit('update-progress-event',{'cell11': dict['cell11'],'cell12': dict['cell12'],'cell13': dict['cell13'],'cell14': dict['cell14']})
+
+@socketio.on('request_progress')
+def request_progress():
+    broadcast_progress()
 
 
 @app.route('/')
-def home():
+def index():
+    """Video streaming home page."""
     return render_template('startPage.html')
-
 
 @app.route('/setmode/', methods=['GET'])
 def set_mode():
@@ -32,15 +67,18 @@ def set_manual_operation():
     return '', 204
 
 
+
+
 @app.route('/startmanual/', methods=['GET'])
 def start_execution_manual():
     return '', 204
 
 
-@app.route('/startauto/', methods=['GET'])
-def start_execution_auto():
-    return '', 204
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5001')
+
+
+
+if __name__ == '__main__':
+    instantiate_flags()
+    socketio.run(app, host="0.0.0.0", port="5001")
--- static/main.js
+++ static/main.js
@@ -1,55 +1,90 @@
+let text11=document.getElementById("cell11");
+let text12=document.getElementById("cell12");
+let text13=document.getElementById("cell13");
+let text14=document.getElementById("cell14");
+
+var socket = io.connect(
+    location.protocol + "//" + document.domain + ":" + location.port
+  );
+
+
+
+
+
+    socket.emit("request_progress");
+    socket.on("update-progress-event", function (data) {
+      console.log(data.img1);
+      text11.innerHTML=data.cell11;
+      text12.innerHTML=data.cell12;
+      text13.innerHTML=data.cell13;
+      text14.innerHTML=data.cell14;
+      socket.emit("request_progress");
+      });
+
+//setInterval(function () {request_progress_from_server();}, 100);
+
+
+
+
 function setModeManual(){
 
-    fetch('/setmode/?m=Manual')
-            .then(function(response) {
-            })
-            .then(function(myJson) {
-            });
-document.getElementById("manualSection").disabled = false;
-document.getElementById("autoSection").disabled = true;
-document.getElementById("navbarDarkDropdownMenuLink").innerHTML = "<b>Manual</b>";
+        fetch('/setmode/?m=Manual')
+                .then(function(response) {
+                })
+                .then(function(myJson) {
+                });
+ document.getElementById("manualSection").disabled = false;
+ document.getElementById("autoSection").disabled = true;
+ document.getElementById("navbarDarkDropdownMenuLink").innerHTML = "<b>Manual</b>";
 }
 function setModeAuto(){
-    fetch('/setmode/?m=Auto')
-            .then(function(response) {
-            })
-            .then(function(myJson) {
-            });
-document.getElementById("manualSection").disabled = true;
-document.getElementById("autoSection").disabled = false;
-document.getElementById("navbarDarkDropdownMenuLink").innerHTML = "<b>Auto</b>";
+        fetch('/setmode/?m=Auto')
+                .then(function(response) {
+                })
+                .then(function(myJson) {
+                });
+ document.getElementById("manualSection").disabled = true;
+ document.getElementById("autoSection").disabled = false;
+ document.getElementById("navbarDarkDropdownMenuLink").innerHTML = "<b>Auto</b>";
 }
 function setInsertionOperation(){
-    fetch('/setmanualoperation/?o=Insertion')
-            .then(function(response) {
-            })
-            .then(function(myJson) {
-            });
-    document.getElementById("dropdownMenuButton1").innerHTML = "Insertion";
+        fetch('/setmanualoperation/?o=Insertion')
+                .then(function(response) {
+                })
+                .then(function(myJson) {
+                });
+        document.getElementById("dropdownMenuButton1").innerHTML = "Insertion";
 
 }
 function setCrimpingOperation(){
-    fetch('/setmanualoperation/?o=Crimping')
-            .then(function(response) {
-            })
-            .then(function(myJson) {
-            });
-    document.getElementById("dropdownMenuButton1").innerHTML = "Crimping";
+        fetch('/setmanualoperation/?o=Crimping')
+                .then(function(response) {
+                })
+                .then(function(myJson) {
+                });
+        document.getElementById("dropdownMenuButton1").innerHTML = "Crimping";
 }
 function startManual(){
 
-    fetch('/startmanual/')
-            .then(function(response) {
-            })
-            .then(function(myJson) {
-            });
+        fetch('/startmanual/')
+                .then(function(response) {
+                })
+                .then(function(myJson) {
+                });
 
-            }
+                }
 function startAuto(){
-    fetch('/startauto/')
-            .then(function(response) {
-            })
-            .then(function(myJson) {
-            });
+        fetch('http://127.0.0.1:5002/startrobot/')
+                .then(function(response) {
+                })
+                .then(function(myJson) {
+                });
 
+}
+function forceQuit(){
+        fetch('http://127.0.0.1:5002/haltrobot/')
+                .then(function(response) {
+                })
+                .then(function(myJson) {
+                });
 }
